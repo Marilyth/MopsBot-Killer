8221d6e661c7c8163152e6044b5dafe1612aaca5
Marilyth <baermay98@gmail.com>
2018-09-18 08:47:55 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MopsBot_Killer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

caeb7fee911c4549f1417d0cd21d7c1280efd022
Marilyth <baermay98@gmail.com>
2018-12-23 19:19:05 +0100

    Increase limit due to Asp.Net

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace MopsKiller
 
         //Open file handling
         private int ProcessId, OpenFilesCount, OpenFilesRepetition, LimitExceededCount;
-        private static int REPETITIONTHRESHOLD = 10, OPENFILESLIMIT = 600;
+        private static int REPETITIONTHRESHOLD = 10, OPENFILESLIMIT = 800;
 
         private async Task Start()
         {
